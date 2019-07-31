5195b60890f64815325171d1564ed1060cdf3b23
Luiz Fernando Assis <luizffga@gmail.com>
2019-07-31 18:26:25 -0300

    create feature value object

---
+++ redis-feeder/src/main/java/info/terrabrasilis/redis/feeder/vo/FeatureVO.java
@@ -0,0 +1,51 @@
+package info.terrabrasilis.redis.feeder.vo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import info.terrabrasilis.redis.feeder.interfaces.Data;
+
+/**
+ * 
+ * @author jether
+ *
+ */
+public final class FeatureVO implements Data, Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3705371968764723429L;
+
+	private Integer loi;
+	private Integer loiname;
+	private List<AreaVO> areas = new ArrayList<>();
+
+	public FeatureVO(Integer loi, Integer loiname, AreaVO... areas) {
+		super();
+		this.loi = loi;
+		this.loiname = loiname;
+
+		this.addAreasToAreasList(areas);
+	}
+
+	public Integer getLoi() {
+		return loi;
+	}
+
+	public Integer getLoiname() {
+		return loiname;
+	}
+
+	public List<AreaVO> getAreas() {
+		return areas;
+	}
+
+	private void addAreasToAreasList(AreaVO[] areas) {
+
+		for (AreaVO area : areas) {
+			this.areas.add(area);
+		}
+	}
+}
