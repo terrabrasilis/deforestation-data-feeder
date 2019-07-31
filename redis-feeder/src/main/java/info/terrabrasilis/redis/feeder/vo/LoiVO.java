934daa41f1a896b49294191f5dad252ad4cfc3c9
Luiz Fernando Assis <luizffga@gmail.com>
2019-07-31 18:26:49 -0300

    create loi value object

---
+++ redis-feeder/src/main/java/info/terrabrasilis/redis/feeder/vo/LoiVO.java
@@ -0,0 +1,70 @@
+package info.terrabrasilis.redis.feeder.vo;
+
+import java.io.Serializable;
+import java.util.List;
+
+import info.terrabrasilis.redis.feeder.interfaces.Data;
+
+/**
+ * 
+ * @author jether
+ *
+ */
+public final class LoiVO implements Data, Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6856573995718568082L;
+
+	private Integer gid;
+	private String name;
+	private List<Data> loinames;
+
+	
+	/**
+	 * @param gid
+	 * @param name
+	 */
+	public LoiVO(Integer gid, String name) {
+		super();
+		this.gid = gid;
+		this.name = name;
+	}
+
+	/**
+	 * @param name
+	 * @param loinames
+	 */
+	public LoiVO(String name, List<Data> loinames) {
+		super();
+		this.name = name;
+		this.loinames = loinames;
+	}
+	
+	/**
+	 * 
+	 * @param gid
+	 * @param name
+	 * @param loinames
+	 */
+	public LoiVO(Integer gid, String name, List<Data> loinames) {
+		super();
+		this.gid = gid;
+		this.name = name;
+		this.loinames = loinames;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public List<Data> getLoinames() {
+		return loinames;
+	}
+
+	public Integer getGid() {
+		return gid;
+	}
+
+}
