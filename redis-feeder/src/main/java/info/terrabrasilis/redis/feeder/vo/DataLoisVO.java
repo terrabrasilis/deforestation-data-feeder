d996c141802ae7679f268ff10e6c6c2a64131ccf
Luiz Fernando Assis <luizffga@gmail.com>
2019-07-31 18:16:13 -0300

    create data lois value object

---
+++ redis-feeder/src/main/java/info/terrabrasilis/redis/feeder/vo/DataLoisVO.java
@@ -0,0 +1,36 @@
+package info.terrabrasilis.redis.feeder.vo;
+
+import java.io.Serializable;
+import java.util.List;
+
+import info.terrabrasilis.redis.feeder.domain.Data;
+
+/**
+ * 
+ * @author jether
+ *
+ */
+public final class DataLoisVO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3505053332629270889L;
+	
+	private Data data;
+	private List<LoiVO> lois;
+	
+	public DataLoisVO(Data data, List<LoiVO> lois) {
+		this.data = data;
+		this.lois = lois;
+	}
+
+	public Data getData() {
+		return data;
+	}
+
+	public List<LoiVO> getLois() {
+		return lois;
+	}
+
+}
