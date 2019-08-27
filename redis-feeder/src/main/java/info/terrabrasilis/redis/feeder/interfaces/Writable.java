f2590e37477b017ae425369bf8545a27a37abd89
Luiz Fernando Assis <luizffga@gmail.com>
2019-08-27 16:18:45 -0300

    include interfaces

--- redis-feeder/src/main/java/info/terrabrasilis/redis/feeder/interfaces/Writable.java
+++ redis-feeder/src/main/java/info/terrabrasilis/redis/feeder/interfaces/Writable.java
@@ -1,11 +1,14 @@
 package info.terrabrasilis.redis.feeder.interfaces;
 
+import java.io.Closeable;
+import java.io.IOException;
+
 /**
  * 
  * @author jether
  *
  */
-public interface Writable {
+public interface Writable extends Closeable {
 	public default void write(String key, Object obj) throws Exception {
 		
 	};
@@ -17,4 +20,13 @@ public interface Writable {
 	public default void write(Object obj, String host, String header) throws Exception {
 		
 	};
+
+	public default void write(Object obj, String host, String header, boolean useGson) throws Exception {
+		
+	};
+
+	@Override
+	public default void close() throws IOException {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 }
