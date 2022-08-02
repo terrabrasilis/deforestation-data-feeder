dc50e444933b77ec8aede83f2b38b83b1dcc5636
andre-carvalho <afacarvalho@yahoo.com.br>
2022-08-02 15:02:28 -0300

    Adding new GeoJSONs of auxiliar maps

--- data-api/LOIS_geojson_for_map/CURL_POST_by_loinames.sh
+++ data-api/LOIS_geojson_for_map/CURL_POST_by_loinames.sh
@@ -1,13 +1,4 @@
-curl -X POST -d @amazon_uf.json http://localhost:3000/dashboard/api/v1/redis-cli/config/uf -H "App-Identifier:prodes_amazon" -H "Content-Type: application/json"
-
-curl -X POST -d @amazon_mun.json http://localhost:3000/dashboard/api/v1/redis-cli/config/mun -H "App-Identifier:prodes_amazon" -H "Content-Type: application/json"
-
-curl -X POST -d @amazon_consunit.json http://localhost:3000/dashboard/api/v1/redis-cli/config/consunit -H "App-Identifier:prodes_amazon" -H "Content-Type: application/json"
-
-curl -X POST -d @amazon_indi.json http://localhost:3000/dashboard/api/v1/redis-cli/config/indi -H "App-Identifier:prodes_amazon" -H "Content-Type: application/json"
-
-#curl -X POST -d @amazon_pathrow.json http://localhost:3000/dashboard/api/v1/redis-cli/config/pathrow -H "App-Identifier:prodes_amazon" -H "Content-Type: application/json"
-
+# CERRADO
 
 curl -X POST -d @cerrado_uf.json http://localhost:3000/dashboard/api/v1/redis-cli/config/uf -H "App-Identifier:prodes_cerrado" -H "Content-Type: application/json"
 
@@ -17,8 +8,7 @@ curl -X POST -d @cerrado_consunit.json http://localhost:3000/dashboard/api/v1/re
 
 curl -X POST -d @cerrado_indi.json http://localhost:3000/dashboard/api/v1/redis-cli/config/indi -H "App-Identifier:prodes_cerrado" -H "Content-Type: application/json"
 
-#curl -X POST -d @cerrado_pathrow.json http://localhost:3000/dashboard/api/v1/redis-cli/config/pathrow -H "App-Identifier:prodes_cerrado" -H "Content-Type: application/json"
-
+# LEGAL AMAZON
 
 curl -X POST -d @legal_amazon_uf.json http://localhost:3000/dashboard/api/v1/redis-cli/config/uf -H "App-Identifier:prodes_legal_amazon" -H "Content-Type: application/json"
 
@@ -28,4 +18,52 @@ curl -X POST -d @legal_amazon_consunit.json http://localhost:3000/dashboard/api/
 
 curl -X POST -d @legal_amazon_indi.json http://localhost:3000/dashboard/api/v1/redis-cli/config/indi -H "App-Identifier:prodes_legal_amazon" -H "Content-Type: application/json"
 
-#curl -X POST -d @legal_amazon_pathrow.json http://localhost:3000/dashboard/api/v1/redis-cli/config/pathrow -H "App-Identifier:prodes_legal_amazon" -H "Content-Type: application/json"
+# AMAZON
+
+curl -X POST -d @amazon_uf.json http://localhost:3000/dashboard/api/v1/redis-cli/config/uf -H "App-Identifier:prodes_amazon" -H "Content-Type: application/json"
+
+curl -X POST -d @amazon_mun.json http://localhost:3000/dashboard/api/v1/redis-cli/config/mun -H "App-Identifier:prodes_amazon" -H "Content-Type: application/json"
+
+curl -X POST -d @amazon_consunit.json http://localhost:3000/dashboard/api/v1/redis-cli/config/consunit -H "App-Identifier:prodes_amazon" -H "Content-Type: application/json"
+
+curl -X POST -d @amazon_indi.json http://localhost:3000/dashboard/api/v1/redis-cli/config/indi -H "App-Identifier:prodes_amazon" -H "Content-Type: application/json"
+
+# CAATINGA
+
+curl -X POST -d @caatinga_uf.json http://localhost:3000/dashboard/api/v1/redis-cli/config/uf -H "App-Identifier:prodes_caatinga" -H "Content-Type: application/json"
+
+curl -X POST -d @caatinga_mun.json http://localhost:3000/dashboard/api/v1/redis-cli/config/mun -H "App-Identifier:prodes_caatinga" -H "Content-Type: application/json"
+
+curl -X POST -d @caatinga_consunit.json http://localhost:3000/dashboard/api/v1/redis-cli/config/consunit -H "App-Identifier:prodes_caatinga" -H "Content-Type: application/json"
+
+curl -X POST -d @caatinga_indi.json http://localhost:3000/dashboard/api/v1/redis-cli/config/indi -H "App-Identifier:prodes_caatinga" -H "Content-Type: application/json"
+
+# PAMPA
+
+curl -X POST -d @pampa_uf.json http://localhost:3000/dashboard/api/v1/redis-cli/config/uf -H "App-Identifier:prodes_pampa" -H "Content-Type: application/json"
+
+curl -X POST -d @pampa_mun.json http://localhost:3000/dashboard/api/v1/redis-cli/config/mun -H "App-Identifier:prodes_pampa" -H "Content-Type: application/json"
+
+curl -X POST -d @pampa_consunit.json http://localhost:3000/dashboard/api/v1/redis-cli/config/consunit -H "App-Identifier:prodes_pampa" -H "Content-Type: application/json"
+
+curl -X POST -d @pampa_indi.json http://localhost:3000/dashboard/api/v1/redis-cli/config/indi -H "App-Identifier:prodes_pampa" -H "Content-Type: application/json"
+
+# MATA ATLANTICA
+
+curl -X POST -d @mata_atlantica_uf.json http://localhost:3000/dashboard/api/v1/redis-cli/config/uf -H "App-Identifier:prodes_mata_atlantica" -H "Content-Type: application/json"
+
+curl -X POST -d @mata_atlantica_mun.json http://localhost:3000/dashboard/api/v1/redis-cli/config/mun -H "App-Identifier:prodes_mata_atlantica" -H "Content-Type: application/json"
+
+curl -X POST -d @mata_atlantica_consunit.json http://localhost:3000/dashboard/api/v1/redis-cli/config/consunit -H "App-Identifier:prodes_mata_atlantica" -H "Content-Type: application/json"
+
+curl -X POST -d @mata_atlantica_indi.json http://localhost:3000/dashboard/api/v1/redis-cli/config/indi -H "App-Identifier:prodes_mata_atlantica" -H "Content-Type: application/json"
+
+# PANTANAL
+
+curl -X POST -d @pantanal_uf.json http://localhost:3000/dashboard/api/v1/redis-cli/config/uf -H "App-Identifier:prodes_pantanal" -H "Content-Type: application/json"
+
+curl -X POST -d @pantanal_mun.json http://localhost:3000/dashboard/api/v1/redis-cli/config/mun -H "App-Identifier:prodes_pantanal" -H "Content-Type: application/json"
+
+curl -X POST -d @pantanal_consunit.json http://localhost:3000/dashboard/api/v1/redis-cli/config/consunit -H "App-Identifier:prodes_pantanal" -H "Content-Type: application/json"
+
+curl -X POST -d @pantanal_indi.json http://localhost:3000/dashboard/api/v1/redis-cli/config/indi -H "App-Identifier:prodes_pantanal" -H "Content-Type: application/json"
--- data-api/LOIS_geojson_for_map/README.md
+++ data-api/LOIS_geojson_for_map/README.md
@@ -6,3 +6,6 @@ Para gerar os GeoJSONs a partir dos shapefiles de LOIs.
 - No proximo passo, configurar para 5% a simplificação;
 - Em seguida usar o "Repair" para reparar eventuais erros após a simplificação;
 - Por fim, exportar para GeoJSON;
+
+Todos os shapesfiles devem possuir apenas a coluna "name" com o nome da feição representada. Deve ser o mesmo nome do dado auxiliar utilizado no processo de preparação do banco DashboardDataModel, mas sem a concatenação com o nome de Estado, no caso do dado de município.
+
