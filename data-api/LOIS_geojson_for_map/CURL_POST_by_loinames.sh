f03a0489e81570f8c597f1530be39f70900ddfb4
andre-carvalho <afacarvalho@yahoo.com.br>
2024-06-11 17:28:13 -0300

    Fix #15

--- data-api/LOIS_geojson_for_map/CURL_POST_by_loinames.sh
+++ data-api/LOIS_geojson_for_map/CURL_POST_by_loinames.sh
@@ -1,81 +1,19 @@
-# CERRADO
+#!/bin/bash
+#
+# list of LOIs
+BASE_LOIS=("cerrado" "legal_amazon" "amazon" "caatinga" "pampa" "pantanal" "mata_atlantica" "amazon_nf")
 
-curl -X POST -d @cerrado_uf.json http://localhost:3000/dashboard/api/v1/redis-cli/config/uf -H "App-Identifier:prodes_cerrado" -H "Content-Type: application/json"
+for BASE_LOI in ${BASE_LOIS[@]}
+do
 
-curl -X POST -d @cerrado_mun.json http://localhost:3000/dashboard/api/v1/redis-cli/config/mun -H "App-Identifier:prodes_cerrado" -H "Content-Type: application/json"
+LOIS=("uf" "mun" "consunit" "indi")
 
-curl -X POST -d @cerrado_consunit.json http://localhost:3000/dashboard/api/v1/redis-cli/config/consunit -H "App-Identifier:prodes_cerrado" -H "Content-Type: application/json"
+    for LOI in ${LOIS[@]}
+    do
 
-curl -X POST -d @cerrado_indi.json http://localhost:3000/dashboard/api/v1/redis-cli/config/indi -H "App-Identifier:prodes_cerrado" -H "Content-Type: application/json"
+        curl -X POST -d @${BASE_LOI}_${LOI}.json http://localhost:3000/dashboard/api/v1/redis-cli/config/${LOI} \
+        -H "App-Identifier:prodes_${BASE_LOI}" -H "Content-Type: application/json Charset=utf-8"
 
-# LEGAL AMAZON
+    done
 
-curl -X POST -d @legal_amazon_uf.json http://localhost:3000/dashboard/api/v1/redis-cli/config/uf -H "App-Identifier:prodes_legal_amazon" -H "Content-Type: application/json"
-
-curl -X POST -d @legal_amazon_mun.json http://localhost:3000/dashboard/api/v1/redis-cli/config/mun -H "App-Identifier:prodes_legal_amazon" -H "Content-Type: application/json"
-
-curl -X POST -d @legal_amazon_consunit.json http://localhost:3000/dashboard/api/v1/redis-cli/config/consunit -H "App-Identifier:prodes_legal_amazon" -H "Content-Type: application/json"
-
-curl -X POST -d @legal_amazon_indi.json http://localhost:3000/dashboard/api/v1/redis-cli/config/indi -H "App-Identifier:prodes_legal_amazon" -H "Content-Type: application/json"
-
-# AMAZON
-
-curl -X POST -d @amazon_uf.json http://localhost:3000/dashboard/api/v1/redis-cli/config/uf -H "App-Identifier:prodes_amazon" -H "Content-Type: application/json"
-
-curl -X POST -d @amazon_mun.json http://localhost:3000/dashboard/api/v1/redis-cli/config/mun -H "App-Identifier:prodes_amazon" -H "Content-Type: application/json"
-
-curl -X POST -d @amazon_consunit.json http://localhost:3000/dashboard/api/v1/redis-cli/config/consunit -H "App-Identifier:prodes_amazon" -H "Content-Type: application/json"
-
-curl -X POST -d @amazon_indi.json http://localhost:3000/dashboard/api/v1/redis-cli/config/indi -H "App-Identifier:prodes_amazon" -H "Content-Type: application/json"
-
-# AMAZON NF
-
-curl -X POST -d @amazon_nf_uf.json http://localhost:3000/dashboard/api/v1/redis-cli/config/uf -H "App-Identifier:prodes_amazon_nf" -H "Content-Type: application/json"
-
-curl -X POST -d @amazon_nf_mun.json http://localhost:3000/dashboard/api/v1/redis-cli/config/mun -H "App-Identifier:prodes_amazon_nf" -H "Content-Type: application/json"
-
-curl -X POST -d @amazon_nf_consunit.json http://localhost:3000/dashboard/api/v1/redis-cli/config/consunit -H "App-Identifier:prodes_amazon_nf" -H "Content-Type: application/json"
-
-curl -X POST -d @amazon_nf_indi.json http://localhost:3000/dashboard/api/v1/redis-cli/config/indi -H "App-Identifier:prodes_amazon_nf" -H "Content-Type: application/json"
-
-
-# CAATINGA
-
-curl -X POST -d @caatinga_uf.json http://localhost:3000/dashboard/api/v1/redis-cli/config/uf -H "App-Identifier:prodes_caatinga" -H "Content-Type: application/json"
-
-curl -X POST -d @caatinga_mun.json http://localhost:3000/dashboard/api/v1/redis-cli/config/mun -H "App-Identifier:prodes_caatinga" -H "Content-Type: application/json"
-
-curl -X POST -d @caatinga_consunit.json http://localhost:3000/dashboard/api/v1/redis-cli/config/consunit -H "App-Identifier:prodes_caatinga" -H "Content-Type: application/json"
-
-curl -X POST -d @caatinga_indi.json http://localhost:3000/dashboard/api/v1/redis-cli/config/indi -H "App-Identifier:prodes_caatinga" -H "Content-Type: application/json"
-
-# PAMPA
-
-curl -X POST -d @pampa_uf.json http://localhost:3000/dashboard/api/v1/redis-cli/config/uf -H "App-Identifier:prodes_pampa" -H "Content-Type: application/json"
-
-curl -X POST -d @pampa_mun.json http://localhost:3000/dashboard/api/v1/redis-cli/config/mun -H "App-Identifier:prodes_pampa" -H "Content-Type: application/json"
-
-curl -X POST -d @pampa_consunit.json http://localhost:3000/dashboard/api/v1/redis-cli/config/consunit -H "App-Identifier:prodes_pampa" -H "Content-Type: application/json"
-
-curl -X POST -d @pampa_indi.json http://localhost:3000/dashboard/api/v1/redis-cli/config/indi -H "App-Identifier:prodes_pampa" -H "Content-Type: application/json"
-
-# MATA ATLANTICA
-
-curl -X POST -d @mata_atlantica_uf.json http://localhost:3000/dashboard/api/v1/redis-cli/config/uf -H "App-Identifier:prodes_mata_atlantica" -H "Content-Type: application/json"
-
-curl -X POST -d @mata_atlantica_mun.json http://localhost:3000/dashboard/api/v1/redis-cli/config/mun -H "App-Identifier:prodes_mata_atlantica" -H "Content-Type: application/json"
-
-curl -X POST -d @mata_atlantica_consunit.json http://localhost:3000/dashboard/api/v1/redis-cli/config/consunit -H "App-Identifier:prodes_mata_atlantica" -H "Content-Type: application/json"
-
-curl -X POST -d @mata_atlantica_indi.json http://localhost:3000/dashboard/api/v1/redis-cli/config/indi -H "App-Identifier:prodes_mata_atlantica" -H "Content-Type: application/json"
-
-# PANTANAL
-
-curl -X POST -d @pantanal_uf.json http://localhost:3000/dashboard/api/v1/redis-cli/config/uf -H "App-Identifier:prodes_pantanal" -H "Content-Type: application/json"
-
-curl -X POST -d @pantanal_mun.json http://localhost:3000/dashboard/api/v1/redis-cli/config/mun -H "App-Identifier:prodes_pantanal" -H "Content-Type: application/json"
-
-curl -X POST -d @pantanal_consunit.json http://localhost:3000/dashboard/api/v1/redis-cli/config/consunit -H "App-Identifier:prodes_pantanal" -H "Content-Type: application/json"
-
-curl -X POST -d @pantanal_indi.json http://localhost:3000/dashboard/api/v1/redis-cli/config/indi -H "App-Identifier:prodes_pantanal" -H "Content-Type: application/json"
-
+done
