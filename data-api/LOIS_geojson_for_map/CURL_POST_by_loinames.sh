63ecf0545cf3a441d1239b39baf6b61787408b6e
andre-carvalho <afacarvalho@yahoo.com.br>
2024-06-12 19:06:07 -0300

    Fix #15

--- data-api/LOIS_geojson_for_map/CURL_POST_by_loinames.sh
+++ data-api/LOIS_geojson_for_map/CURL_POST_by_loinames.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 #
+DATA_DIR=`pwd`
+#
 # list of LOIs
 BASE_LOIS=("cerrado" "legal_amazon" "amazon" "caatinga" "pampa" "pantanal" "mata_atlantica" "amazon_nf")
 
 for BASE_LOI in ${BASE_LOIS[@]}
 do
 
-LOIS=("uf" "mun" "consunit" "indi")
+    docker-compose -f ${DATA_DIR}/../../docker-compose/docker-compose.yml up -d
+    sleep 10
+
+    LOIS=("uf" "mun" "consunit" "indi")
 
     for LOI in ${LOIS[@]}
     do
 
         curl -X POST -d @${BASE_LOI}_${LOI}.json http://localhost:3000/dashboard/api/v1/redis-cli/config/${LOI} \
-        -H "App-Identifier:prodes_${BASE_LOI}" -H "Content-Type: application/json Charset=utf-8"
-
+        -H "App-Identifier:prodes_${BASE_LOI}" -H "Content-Type: application/json"
+        
+        sleep 2
     done
+
+    docker-compose -f ${DATA_DIR}/../../docker-compose/docker-compose.yml down
 
 done
