84aa8fc2ee93e97a965c4fad314316d8c6a30532
andre-carvalho <afacarvalho@yahoo.com.br>
2024-07-03 16:33:47 -0300

    New Json files for LOIs. Change the upload script for work with new structure of directory and file names.

--- data-api/LOIS_geojson_for_map/CURL_POST_by_loinames.sh
+++ data-api/LOIS_geojson_for_map/CURL_POST_by_loinames.sh
@@ -2,22 +2,28 @@
 #
 DATA_DIR=`pwd`
 #
-# list of LOIs
-BASE_LOIS=("cerrado" "legal_amazon" "amazon" "caatinga" "pampa" "pantanal" "mata_atlantica" "amazon_nf")
+# list of locations for each JSON LOIs
+BASE_LOIS=("cerrado" "amazonia_legal" "amazonia" "caatinga" "pampa" "pantanal" "mata_atlantica" "amazonia_nf")
+# list of App Identifier for each LOI as Header
+APP_ID_LOIS=("cerrado" "legal_amazon" "amazon" "caatinga" "pampa" "pantanal" "mata_atlantica" "amazon_nf")
 
-for BASE_LOI in ${BASE_LOIS[@]}
+length_j=${#BASE_LOIS[@]}
+for ((j=0; j<$length_j; ++j));
 do
 
     docker-compose -f ${DATA_DIR}/../../docker-compose/docker-compose.yml up -d
     sleep 10
 
-    LOIS=("uf" "mun" "consunit" "indi")
+    # The name for each LOI in config API
+    CONFIG_LOIS=("uf" "mun" "consunit" "indi")
+    # The name for each JSON file LOI
+    JSON_LOIS=("state" "municipality" "uc" "ti")
 
-    for LOI in ${LOIS[@]}
+    length=${#CONFIG_LOIS[@]}
+    for ((i=0; i<$length; ++i));
     do
-
-        curl -X POST -d @${BASE_LOI}_${LOI}.json http://localhost:3000/dashboard/api/v1/redis-cli/config/${LOI} \
-        -H "App-Identifier:prodes_${BASE_LOI}" -H "Content-Type: application/json"
+        curl -X POST -d @${BASE_LOIS[$j]}/${JSON_LOIS[$i]}.json http://localhost:3000/dashboard/api/v1/redis-cli/config/${CONFIG_LOIS[$i]} \
+        -H "App-Identifier:prodes_${APP_ID_LOIS[$j]}" -H "Content-Type: application/json"
         
         sleep 2
     done
